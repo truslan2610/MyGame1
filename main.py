19fc4833d01f6ad063ec16afe95832d6b254a333
ruslan.tkachenko <truslan@gmail.com>
2024-03-03 12:27:29 +0200

    added snake

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import pygame, sys
 WIDTH = 800
 HEIGTH = 600
 
+class Snake:
+    head_pos = [10, 10]
+
+    def __init__(self):
+        head_pos = [10, 10]
+
+    def draw_snake(self, game):
+        pygame.draw.rect(game.screen, 'white', (self.head_pos[0], self.head_pos[1], 10, 10))
+
+    def move_snake(self):
+        self.head_pos[1] += 10
 
 class Game:
     def __int__(self):
@@ -12,6 +23,10 @@ class Game:
         self.clock = pygame.time.Clock()
 
     def run(self):
+        snake = Snake()
+        snake.__init__()
+        snake.draw_snake(game)
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -19,7 +34,10 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            snake.move_snake()
+            snake.draw_snake(game)
             pygame.display.update()
+            pygame.time.wait(1000)
 
 
 if __name__ == '__main__':
